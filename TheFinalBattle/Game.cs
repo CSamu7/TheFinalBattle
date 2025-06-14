6d488ed77d4f186c2106ef38da78b53d730cfab7
CSamu7 <samuel.pdg@hotmail.com>
2025-06-14 15:39:11 -0600

    Clase Game añadida.

---
+++ TheFinalBattle/Game.cs
@@ -0,0 +1,26 @@
+﻿namespace TheFinalBattle
+{
+    public class Game
+    {
+        public void Start()
+        {
+            ConfigParty configParty = new ConfigParty();
+
+            Console.WriteLine("Configure the game: ");
+            Party heroes = configParty.CreateParty("The player is controlled by (PC, Human): ");
+            Party enemy = configParty.CreateParty("The monsters are controlled by (PC, Human): ");
+
+            heroes.AddItem(new HealthPotion(), 3);
+            enemy.AddItem(new HealthPotion(), 1);
+
+            Console.Write("Enter your name, hero: ");
+            string name = Console.ReadLine() ?? "Kiryu";
+
+            TrueProgrammer trueProgrammer = new TrueProgrammer(name);
+            heroes.AddMember(trueProgrammer);
+
+            Battles battles = new Battles(enemy);
+            battles.StartBattles(heroes);
+        }
+    }
+}
--- TheFinalBattle/Program.cs
+++ TheFinalBattle/Program.cs
@@ -1,20 +1,5 @@
 ﻿using TheFinalBattle;
 
 Console.ForegroundColor = ConsoleColor.White;
-ConfigParty configParty = new ConfigParty();
-
-Console.WriteLine("Configure the game: ");
-Party heroes = configParty.CreateParty("The player is controlled by (PC, Human): ");
-Party enemy = configParty.CreateParty("The monsters are controlled by (PC, Human): ");
-
-heroes.AddItem(new HealthPotion(), 3);
-enemy.AddItem(new HealthPotion(), 1);
-
-Console.Write("Enter your name, hero: ");
-string name = Console.ReadLine() ?? "Kiryu";
-
-TrueProgrammer trueProgrammer = new TrueProgrammer(name);
-heroes.AddMember(trueProgrammer);
-
-Battles battles = new Battles(enemy);
-battles.StartBattles(heroes);
+Game game = new Game();
+game.Start();
