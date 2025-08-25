481011c11a980d49da81fbf5846826a2f4021409
CSamu7 <samuel.pdg@hotmail.com>
2025-08-24 23:13:28 -0600

    Añadido el ataque: Cañon de consolas.

---
+++ TheFinalBattle/Attacks/CannonOfConsolas.cs
@@ -0,0 +1,19 @@
+﻿namespace TheFinalBattle.Attacks
+{
+    public class CannonOfConsolas : IAttack
+    {
+        public string Name => "Cannon Of Consolas";
+        public DamageType DamageType => DamageType.Range;
+        public AttackData CalculateAttack()
+        {
+            Random rnd = new Random();
+
+            int rndNumber = rnd.Next(1, 30);
+
+            if (rndNumber % 5 == 0 && rndNumber % 3 == 0) return new AttackData(3, DamageType);
+            if (rndNumber % 5 == 0 || rndNumber % 3 == 0) return new AttackData(2, DamageType);
+
+            return new AttackData(1, DamageType);
+        }
+    }
+}
